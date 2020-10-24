8033fdeecbb7c05c16858c96d4992b47e5aa2efe
yukosugasuga <yukosugasuga@gmail.com>
2020-10-24 17:09:44 +0900

    Initial commit

---
+++ src/hhkb2020.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    val s = readLine()
+    val t = readLine()
+
+    if (s == "Y") {
+        if (t == "a") {
+            println("A")
+        }
+        if (t == "b") {
+            println("B")
+        }
+        if (t == "c") {
+            println("C")
+        }
+    }
+    else {
+        println(t)
+    }
+}
