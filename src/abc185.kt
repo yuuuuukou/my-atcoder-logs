41c313b2e51f458c2326a6cbeac6f6e86d3148e2
yukosugasuga <yukosugasuga@gmail.com>
2021-01-20 23:07:10 +0900

    fix abc185 b(TLE)

--- src/abc185.kt
+++ src/abc185.kt
@@ -1,3 +1,4 @@
+import kotlin.math.min
 import kotlin.system.exitProcess
 
 fun main() {
@@ -12,47 +13,31 @@ fun solveABC185B() {
     // B: カフェto
     val (n, m, t) = readLine()!!.split(" ").map { it.toInt() }
 
-    // バッテリーの増減を保持
-    val map = mutableMapOf<Double, Int>()
+    var current = n
+    var lastTime = 0
 
-    // 基本はバッテリーが減る
-    for (i in 0 until t) {
-        map[i + 0.5] = -1
-    }
-
-    // カフェにいる間は増える
-    for (i in 0 until m) {
-        val (a, b) = readLine()!!.split(" ").map { it.toInt() }
-        for (j in a until b) {
-            map[j + 0.5] = 1
+    for (i in 0..m) {
+        val (a, b) = if (i == m) {
+            // ループの最後は帰宅時刻まで引っ張る
+            listOf(t, t)
+        } else {
+            readLine()!!.split(" ").map { it.toInt() }
         }
-    }
 
-    // 増減確認
-    var battery = n
-    for (i in 0 .. t) {
-        if (battery <= 0) {
-            // 途中で充電切れ
+        // current->aまで減算
+        current -= (a - lastTime)
+        if (current <= 0) {
             print("No")
             exitProcess(0)
         }
 
-        if ((map[i + 0.5] ?: 0) > 0) {
-            // 加算
-            if (battery >= n) {
-                // バッテリー容量Nを超えては充電不可
-            } else {
-                battery += (map[i + 0.5] ?: 0)
-            }
-        } else {
-            // 減算
-            battery += (map[i + 0.5] ?: 0)
-        }
+        // b->aまで分を加算(nが上限)
+        current = min(n, current + b - a)
+
+        lastTime = b
     }
 
-    // 充電切れなければYes
     print("Yes")
-
 }
 
 fun solveABC185A() {
