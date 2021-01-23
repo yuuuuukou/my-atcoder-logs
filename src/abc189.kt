e7ad0a32dd32ef6a7b48c1fc161a05bfb75895c0
yukosugasuga <yukosugasuga@gmail.com>
2021-01-23 23:29:27 +0900

    solve abc189 c

--- src/abc189.kt
+++ src/abc189.kt
@@ -1,9 +1,28 @@
+import java.lang.Integer.min
 import java.math.BigDecimal
-import kotlin.math.absoluteValue
+import kotlin.math.max
 import kotlin.system.exitProcess
 
 fun main() {
-    solveABC189B()
+    solveABC189C()
+}
+
+fun solveABC189C() {
+    val n = readLine()!!.toInt()
+    val a = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
+
+    var ans = 0
+    for (l in 0 until n) {
+        // xの初期値をl番目の値で設定
+        var x = a[l]
+        for (r in l until n) {
+            // xをminで更新
+            x = min(x, a[r])
+            // lからrまでの分、xをかける
+            ans = max(ans, x * (r - l + 1))
+        }
+    }
+    println(ans)
 }
 
 fun solveABC189B() {
