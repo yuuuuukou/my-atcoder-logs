568c3541a2b40c9a9c3b908b7d2f5be4cc55cd57
yukosugasuga <yukosugasuga@gmail.com>
2021-01-26 23:12:33 +0900

    solve abc156 c

---
+++ src/abc156.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    solveABC156C()
+}
+
+private fun solveABC156C() {
+    /*val n = */readLine()!!.toInt()
+    val xList = readLine()!!.split(" ").map { it.toInt() }
+
+    var min = Int.MAX_VALUE
+
+    for (p in 1..100) {
+        var sum = 0
+        for (x in xList) {
+            sum += (x - p) * (x - p)
+        }
+        if (sum < min) {
+            min = sum
+        }
+    }
+
+    print(min)
+}
