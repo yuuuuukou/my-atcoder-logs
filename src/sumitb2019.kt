4e5d30638d694d4f9056e8ccdb07472c1b22c49e
yukosugasuga <yukosugasuga@gmail.com>
2021-02-04 07:59:02 +0900

    solve sumitb2019 b

---
+++ src/sumitb2019.kt
@@ -0,0 +1,18 @@
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    solveSumitb2019B()
+}
+
+private fun solveSumitb2019B() {
+    val n = readLine()!!.toInt()
+
+    for (i in 1..n) {
+        val x = (i * 1.08).toInt()
+        if (n == x) {
+            print(i)
+            exitProcess(0)
+        }
+    }
+    print(":(")
+}
