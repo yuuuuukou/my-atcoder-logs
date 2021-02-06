81ac5f2b82377b336fdb2c238fc4e1e7df5f97da
yukosugasuga <yukosugasuga@gmail.com>
2021-02-06 10:22:15 +0900

    solve sumitb2019 c

--- src/abc190.kt
+++ src/abc190.kt
@@ -1,46 +1,70 @@
+import kotlin.math.pow
 import kotlin.math.roundToInt
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-//    solveABC190C()
+    solveABC190C()
 }
 
-//fun solveABC190C() {
-//    // n    : 1..nの番号が付いた 皿
-//    // m    : 1..Mの番号が付いた 条件
-//    // 条件i : 皿Ai,皿Biの両方にボールが置かれているときに満たされる
-//    // k    : 1..kの番号が付いた 人、人iは皿Ciか皿Diのどちらか一方にボールを置く
-//
-//    // 皿
-//    var aList = mutableListOf<Int>()
-//    var bList = mutableListOf<Int>()
-//
-//    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
-//    for (i in 1..m) {
-//        val (ai, bi) = readLine()!!.split(" ").map { it.toInt() }
-//        aList.add(ai)
-//        bList.add(bi)
-//    }
-//
-//    // 人
-//    var cList = mutableListOf<Int>()
-//    var dList = mutableListOf<Int>()
-//    val k = readLine()!!.toInt()
-//    for (i in 1..k) {
-//        val (ci, di) = readLine()!!.split(" ").map { it.toInt() }
-//        cList.add(ci)
-//        dList.add(di)
-//    }
-//
-//    val patternList = mutableListOf<Int>()
-//
-//    for (i in 1 .. Math.pow(2.0, k.toDouble()).roundToInt()) {
-//        for (j in 0 until k) {
-//
-//        }
-//    }
-//
-//}
+fun solveABC190C() {
+    // n    : 1..nの番号が付いた 皿
+    // m    : 1..Mの番号が付いた 条件
+    // 条件i : 皿Ai,皿Biの両方にボールが置かれているときに満たされる
+    // k    : 1..kの番号が付いた 人、人iは皿Ciか皿Diのどちらか一方にボールを置く
+
+    // 条件
+    val conditions = mutableListOf<Pair<Int, Int>>()
+    val (_, m) = readLine()!!.split(" ").map { it.toInt() }
+    for (i in 0 until m) {
+        val (ai, bi) = readLine()!!.split(" ").map { it.toInt() }
+        conditions.add(ai to bi)
+    }
+
+    // 人
+    val choices = mutableListOf<Pair<Int, Int>>()
+    val k = readLine()!!.toInt()
+    for (i in 0 until k) {
+        val (ci, di) = readLine()!!.split(" ").map { it.toInt() }
+        choices.add(ci to di)
+    }
+
+    var ans = 0
+    // 2^k のパターンをループ
+    for (bit in 0 until ((2.0).pow(k).roundToInt())) {
+
+        // ボールが乗っているかどうかのmap
+        val balls = mutableMapOf<Int, Boolean>()
+
+        // 人数分の選択を全探索
+        for ((i, choice) in choices.withIndex()) {
+            val (c, d) = choice
+            // bitのi番目が立っていれば    cを選択
+            //           立っていなければ dを選択
+            // -> 1をi桁左シフト(1 shl i)した値とandを取ることで
+            //    i番目のフラグが立っているか確認
+            val selectedBall = if (bit and (1 shl i) == 0) c else d
+
+            // 選んだボールのフラグを立てる
+            // = 皿にボールが乗った状態
+            balls[selectedBall] = true
+        }
+
+        // 条件を確認していく
+        var count = 0
+        for (condition in conditions) {
+            val (a, b) = condition
+            if (balls[a] == true && balls[b] == true) {
+                count++
+            }
+        }
+
+        // count最大のものを解答とする
+        if (ans < count) {
+            ans = count
+        }
+    }
+    println(ans)
+}
 
 fun solveABC190B() {
     // n:魔法の種類
