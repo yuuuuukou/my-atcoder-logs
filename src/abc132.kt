ed72939d2ce9f9ea000834947a4adb957ec42119
yukosugasuga <yukosugasuga@gmail.com>
2021-02-20 17:12:52 +0900

    solve abc132 c

---
+++ src/abc132.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    /*val n = */readLine()!!.toInt()
+    val dList = readLine()!!.split(" ").map { it.toInt() }.sorted()
+
+    val dx = dList[dList.size / 2 - 1]
+    val dy = dList[dList.size / 2]
+
+    println(dy - dx)
+}
