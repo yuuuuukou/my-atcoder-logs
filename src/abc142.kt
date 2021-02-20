2a28e5d49492fd9cea149a903f51964fa7ccf1f9
yukosugasuga <yukosugasuga@gmail.com>
2021-02-20 19:08:08 +0900

    solve abc142 c

---
+++ src/abc142.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    val n = readLine()!!.toInt()
+    val a = readLine()!!.split(" ").map { it.toInt() }
+
+    val map = mutableMapOf<Int, Int>()
+
+    for ((i, ai) in a.withIndex()) {
+        // i+1さん は ai番目
+        map[ai] = i + 1
+    }
+
+    for (i in 1..n) {
+        if (i==n) {
+            print("${map[i]}")
+        } else {
+            print("${map[i]} ")
+        }
+    }
+}
