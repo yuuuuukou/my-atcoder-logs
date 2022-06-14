27f5f236dfd149627a41adef52b452f2251d533a
yukosugasuga <yukosugasuga@gmail.com>
2022-06-15 07:09:26 +0900

    refactor: 問題のメソッド化

--- src/abc086.kt
+++ src/abc086.kt
@@ -2,6 +2,10 @@ import kotlin.math.pow
 import kotlin.math.sqrt
 
 fun main(args: Array<String>) {
+    solveABC086B()
+}
+
+private fun solveABC086B() {
     val ab = readLine()!!.replace(" ", "").toInt()
     val x = sqrt(ab.toDouble()).toInt()
     if (x.toDouble().pow(2) == ab.toDouble()) {
