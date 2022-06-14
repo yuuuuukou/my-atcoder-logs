047f9702a724c8919bcb248a53e2fa3c3652dcd8
yukosugasuga <yukosugasuga@gmail.com>
2022-06-15 07:16:26 +0900

    solve: abc068A
    https://qiita.com/drken/items/fd4e5e3630d0f5859067#%E7%AC%AC-1-%E5%95%8F--abc-086-a---product-100-%E7%82%B9

--- src/abc086.kt
+++ src/abc086.kt
@@ -2,15 +2,24 @@ import kotlin.math.pow
 import kotlin.math.sqrt
 
 fun main(args: Array<String>) {
-    solveABC086B()
+    solveABC086A()
 }
 
-private fun solveABC086B() {
+fun solveABC086B() {
     val ab = readLine()!!.replace(" ", "").toInt()
     val x = sqrt(ab.toDouble()).toInt()
     if (x.toDouble().pow(2) == ab.toDouble()) {
         println("Yes")
     } else {
         println("No")
+    }
+}
+
+fun solveABC086A() {
+    val (a, b) = readLine()!!.split(" ").map { it.toInt() }
+    if ((a * b) % 2 == 0) {
+        println("Even")
+    } else {
+        println("Odd")
     }
 }
