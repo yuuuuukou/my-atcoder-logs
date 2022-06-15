296eed485016acedf44d82c1d851a6ca87a7113d
yukosugasuga <yukosugasuga@gmail.com>
2022-06-15 21:16:13 +0900

    solve: abc081A
    https://qiita.com/drken/items/fd4e5e3630d0f5859067#%E7%AC%AC-2-%E5%95%8F--abc-081-a---placing-marbles-100-%E7%82%B9

---
+++ src/abc081.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    solveABC081A()
+}
+
+fun solveABC081A() {
+    val s = readLine()!!
+
+    println(s[0].toString().toInt() + s[1].toString().toInt() + s[2].toString().toInt())
+}
