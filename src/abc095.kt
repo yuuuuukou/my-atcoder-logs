82e8a695ca0d3eb50a6139c010729e7a9a7f5bc6
yuuuuukou <yuuuuukou@gmail.com>
2022-07-03 23:51:50 +0900

    solve: abc095B
    https://qiita.com/drken/items/fd4e5e3630d0f5859067#%E7%AC%AC-3-%E5%95%8F--abc-081-b---shift-only-200-%E7%82%B9

--- src/abc095.kt
+++ src/abc095.kt
@@ -1,5 +1,28 @@
+import kotlin.math.min
+
 fun main(args: Array<String>) {
-    solveABC095A()
+    solveABC095B()
+}
+
+fun solveABC095B() {
+    val (n, x) = readLine()!!.split(" ").map { it.toInt() }
+
+    var cnt = 0
+    var remain = x
+    var minCost = Int.MAX_VALUE
+    for (i in 0 until n) {
+        val mi = readLine()!!.toInt()
+        remain -= mi
+        minCost = min(minCost, mi)
+        cnt++
+    }
+
+    while (remain >= minCost) {
+        remain -= minCost
+        cnt++
+    }
+
+    println(cnt)
 }
 
 fun solveABC095A() {
