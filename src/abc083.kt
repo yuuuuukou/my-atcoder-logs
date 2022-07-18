5262a55e9a1527ad04091b713dedf1944ea8e6ed
yuuuuukou <yuuuuukou@gmail.com>
2022-07-18 20:36:02 +0900

    solve: abc083B 別解
    https://qiita.com/drken/items/fd4e5e3630d0f5859067#%E7%AC%AC-5-%E5%95%8F--abc-083-b---some-sums-200-%E7%82%B9

--- src/abc083.kt
+++ src/abc083.kt
@@ -6,17 +6,37 @@ fun solveABC083B() {
     val (n, a, b) = readLine()!!.split(" ").map { it.toInt() }
 
     var ans = 0
-    for (i in 1..n) {
-        val iString = i.toString()
-        var sum = 0
-        for (char in iString) {
-            sum += char.toString().toInt()
-        }
 
+//    // 文字列としてループさせる方法
+//    for (i in 1..n) {
+//        val iString = i.toString()
+//        var sum = 0
+//        for (char in iString) {
+//            sum += char.toString().toInt()
+//        }
+//
+//        if (sum in a..b) {
+//            ans += i
+//        }
+//    }
+
+    // 数値として10ずつ割って余りを使っていく方法
+    for (i in 1..n) {
+        val sum = calc(i)
         if (sum in a..b) {
             ans += i
         }
     }
 
     println(ans)
+}
+
+private fun calc(n: Int): Int {
+    var ndash = n
+    var sum = 0
+    while (ndash > 0) {
+        sum += ndash % 10
+        ndash /= 10
+    }
+    return sum
 }
