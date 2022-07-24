83afad3973af04b5c0f09253cbe9f87d6dda6645
yuuuuukou <yuuuuukou@gmail.com>
2022-07-24 22:41:23 +0900

    solve: abc088B(解き直し)
    https://qiita.com/drken/items/fd4e5e3630d0f5859067#%E7%AC%AC-6-%E5%95%8F--abc-088-b---card-game-for-two-200-%E7%82%B9

--- src/abc088.kt
+++ src/abc088.kt
@@ -1,23 +1,20 @@
 fun main(args: Array<String>) {
-    solveABC088A()
+    solveABC088B()
 }
 
 fun solveABC088B() {
-    /*val n = */readLine()!!.toInt()
+    /* val n = */readLine()!!.toInt()
     val a = readLine()!!.split(" ").map { it.toInt() }
 
     var aliceSum = 0
     var bobSum = 0
-
-    val sortedA = a.sortedDescending()
-    for ((i, ai) in sortedA.withIndex()) {
+    for ((i, ai) in a.sortedDescending().withIndex()) {
         if (i % 2 == 0) {
             aliceSum += ai
         } else {
             bobSum += ai
         }
     }
-
     println(aliceSum - bobSum)
 }
 
