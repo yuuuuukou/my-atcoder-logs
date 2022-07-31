247995f53f630f2d84770692f24ebb16363f136e
yuuuuukou <yuuuuukou@gmail.com>
2022-07-31 15:44:23 +0900

    solve: abc085C
    https://qiita.com/drken/items/fd4e5e3630d0f5859067#%E7%AC%AC-8-%E5%95%8F--abc-085-c---otoshidama-300-%E7%82%B9

--- src/abc085.kt
+++ src/abc085.kt
@@ -1,11 +1,28 @@
 fun main(args: Array<String>) {
-    solveABC085B()
+    solveABC085C()
+}
+
+fun solveABC085C() {
+    val (n, y) = readLine()!!.split(" ").map { it.toInt() }
+
+    var ans = "-1 -1 -1"
+    for (i in 0..n) {
+        for (j in 0..n - i) {
+            val k = n - i - j
+
+            if (10000 * i + 5000 * j + 1000 * k == y) {
+                ans = "$i $j $k"
+            }
+        }
+    }
+
+    println(ans)
 }
 
 fun solveABC085B() {
     val n = readLine()!!.toInt()
-    val d = mutableMapOf<Int,Int>()
-    for (i in 0 until n){
+    val d = mutableMapOf<Int, Int>()
+    for (i in 0 until n) {
         val di = readLine()!!.toInt()
         d[di] = 1
     }
