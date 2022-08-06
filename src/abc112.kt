89792e64bcb188e79d14d5fe32d636391937e715
yuuuuukou <yuuuuukou@gmail.com>
2022-08-06 10:20:12 +0900

    solve: abc113C
    https://qiita.com/drken/items/fd4e5e3630d0f5859067#%E7%AC%AC-8-%E5%95%8F--abc-085-c---otoshidama-300-%E7%82%B9

---
+++ src/abc112.kt
@@ -0,0 +1,53 @@
+import kotlin.math.abs
+import kotlin.math.max
+
+fun main(args: Array<String>) {
+    solveABC112C()
+}
+
+fun solveABC112C() {
+    // --------------------------------------------------
+    // ピラミッド
+    // - 中心座標(Cx,Cy)
+    // - 高さh
+    // - 座標(x,y)の高度 max(h - |x - cx| - |y - cy|, 0)
+
+    // 調査結果
+    // - 1 <= n <= 100
+    // - 0 <= xi,yi <= 100
+    // - nの情報 i個目の情報 座標(xi, yi)の高度はhi
+
+    // -> 中心座標と高さを求めたい
+    // --------------------------------------------------
+    data class Research(val x: Long, val y: Long, val h: Long)
+
+    val n = readLine()!!.toInt()
+
+    val researches = mutableListOf<Research>()
+    for (i in 0 until n) {
+        val (x, y, h) = readLine()!!.split(" ").map { it.toLong() }
+        researches.add(Research(x, y, h))
+    }
+
+    for (cx in 0..100) {
+        for (cy in 0..100) {
+            for (research in researches) {
+                // cx, cy を仮定してhを計算
+                val h = abs(research.x - cx) + abs(research.y - cy) + research.h
+                if (h >= 0) {
+                    // 他の調査結果との整合を確認
+                    var flag = true
+                    for (ohterResearch in researches) {
+                        if (ohterResearch.h != max(h - abs(ohterResearch.x - cx) - abs(ohterResearch.y - cy), 0)) {
+                            flag = false
+                        }
+                    }
+                    if (flag) {
+                        println("$cx $cy $h")
+                        return
+                    }
+                }
+            }
+        }
+    }
+}
