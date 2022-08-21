f2c371a64cb53c7dda74185f4198823e4ac0c1cf
yuuuuukou <yuuuuukou@gmail.com>
2022-08-22 01:09:08 +0900

    solve: abc095C
    https://qiita.com/e869120/items/f1c6f98364d1443148b3#1-6-4-%E5%85%A8%E6%8E%A2%E7%B4%A2%E3%81%AB%E6%85%A3%E3%82%8C%E3%82%8B

--- src/abc095.kt
+++ src/abc095.kt
@@ -1,7 +1,29 @@
 import kotlin.math.min
+import kotlin.math.max
 
 fun main(args: Array<String>) {
-    solveABC095B()
+    solveABC095C()
+}
+
+fun solveABC095C() {
+    val (a, b, c, x, y) = readLine()!!.split(" ").map { it.toInt() }
+
+    var cost = Int.MAX_VALUE
+    for (ai in 0..x) {
+        // Aピザの枚数を全探索。必要になるABピザでまかなう必要があるAピザの枚数を逆算
+        val ci = x - ai
+
+        // ci枚分はBピザもまかなわれるので、Bピザ購入数を計算
+        // (負数になる場合既に十分な量になってるのでBピザは不要(=0))
+        val bi = max(y - ci, 0)
+
+        // 金額を試算
+        // ABピザは2枚で1枚分なので *2
+        // BピザはABピザで買ってしまった方が安い可能性があるので、ABピザを購入したケースとminを取る
+        val tmpCost = a * ai + min(b * bi, c * bi * 2) + c * ci * 2
+        cost = min(cost, tmpCost)
+    }
+    println(cost)
 }
 
 fun solveABC095B() {
