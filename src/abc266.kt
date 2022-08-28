6aaeeea76e82df4f35d0ac4d067328d70f39dd50
yuuuuukou <yuuuuukou@gmail.com>
2022-08-28 10:07:22 +0900

    solve: abc266C

--- src/abc266.kt
+++ src/abc266.kt
@@ -1,7 +1,3 @@
-import java.math.RoundingMode
-import kotlin.math.pow
-import kotlin.math.sqrt
-
 private fun readLn() = readLine()!!
 private fun readStrings() = readLn().split(" ").toMutableList()
 private fun readInt() = readLn().toInt()
@@ -9,60 +5,34 @@ private fun readInts() = readLn().split(" ").map { it.toInt() }.toMutableList()
 private fun readLong() = readLn().toLong()
 private fun readLongs() = readLn().split(" ").map { it.toLong() }.toMutableList()
 
-private fun readIntsPlus100() = readLn().split(" ").map { it.toInt() + 100 }.toMutableList()
-
 fun main(args: Array<String>) {
     solveABC266C()
 }
 
 fun solveABC266C() {
-    val (ax, ay) = readIntsPlus100()
-    val (bx, by) = readIntsPlus100()
-    val (cx, cy) = readIntsPlus100()
-    val (dx, dy) = readIntsPlus100()
+    val (ax, ay) = readInts()
+    val (bx, by) = readInts()
+    val (cx, cy) = readInts()
+    val (dx, dy) = readInts()
 
-    // abの距離
-    val ab = sqrt((bx.toDouble() - ax.toDouble()).pow(2.0) + (by.toDouble() - ay.toDouble()).pow(2.0))
-    val bc = sqrt((cx.toDouble() - bx.toDouble()).pow(2.0) + (cy.toDouble() - by.toDouble()).pow(2.0))
-    val cd = sqrt((dx.toDouble() - cx.toDouble()).pow(2.0) + (dy.toDouble() - cy.toDouble()).pow(2.0))
-    val da = sqrt((ax.toDouble() - dx.toDouble()).pow(2.0) + (ay.toDouble() - dy.toDouble()).pow(2.0))
+    // 角Aの外積 bxd
+    val a = calc(bx - ax, by - ay, dx - ax, dy - ay)
+    // 角Bの外積 cxa
+    val b = calc(cx - bx, cy - by, ax - bx, ay - by)
+    // 角Cの外積 dxb
+    val c = calc(dx - cx, dy - cy, bx - cx, by - cy)
+    // 角Dの外積 axc
+    val d = calc(ax - dx, ay - dy, cx - dx, cy - dy)
 
-    // 対角線
-    val ac = sqrt((cx.toDouble() - ax.toDouble()).pow(2.0) + (cy.toDouble() - ay.toDouble()).pow(2.0))
-    val bd = sqrt((dx.toDouble() - bx.toDouble()).pow(2.0) + (dy.toDouble() - by.toDouble()).pow(2.0))
-
-    var isRectangle = true
-
-    if (ab == bc && bc == cd && cd == da) {
-
-    } else {
-        isRectangle = false
-    }
-
-    if (bc / ab == cd / bc && cd / bc == da / cd && da / cd == ab / da) {
-
-    } else {
-        isRectangle = false
-    }
-
-    val acBD = ac.toString().toBigDecimal().setScale(10, RoundingMode.FLOOR)
-    val bdBD = bd.toString().toBigDecimal().setScale(10, RoundingMode.FLOOR)
-
-    if (acBD == bdBD) {
-        val root2 = sqrt(2.0).toString().toBigDecimal().setScale(10, RoundingMode.FLOOR)
-        if (root2 % acBD == 0.toBigDecimal().setScale(10, RoundingMode.FLOOR)) {
-        } else {
-            isRectangle = false
-        }
-    } else {
-        isRectangle = false
-    }
-
-    if (isRectangle) {
+    if (a > 0 && b > 0 && c > 0 && d > 0) {
         println("Yes")
     } else {
         println("No")
     }
+}
+
+private fun calc(ax: Int, ay: Int, bx: Int, by: Int): Int {
+    return ax * by - ay * bx
 }
 
 fun solveABC266B() {
