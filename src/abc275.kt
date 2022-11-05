44c416bd4e841635c2088bfe6d145a2c59bd4d22
yuuuuukou <yuuuuukou@gmail.com>
2022-11-05 10:06:13 +0900

    solve: abc275A, 001-012A

---
+++ src/abc275.kt
@@ -0,0 +1,30 @@
+private val reader = System.`in`.bufferedReader()
+private fun readString() = reader.readLine()
+private fun readStrings() = readString().split(" ").toMutableList()
+private fun readInt() = readString().toInt()
+private fun readInts() = readString().split(" ").map { it.toInt() }.toMutableList()
+private fun readLong() = readString().toLong()
+private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
+private fun readDouble() = readString().toDouble()
+private fun readDoubles() = readString().split(" ").map { it.toDouble() }.toMutableList()
+private fun readBigDecimal() = readString().toBigDecimal()
+private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
+
+fun main(args: Array<String>) {
+    solveABC275A()
+}
+
+fun solveABC275A() {
+    /*val n = */readInt()
+    val h = readInts()
+
+    var max = 0
+    var maxIndex = 0
+    for ((index, hi) in h.withIndex()) {
+        if (max < hi) {
+            max = hi
+            maxIndex = index + 1
+        }
+    }
+    println(maxIndex)
+}
