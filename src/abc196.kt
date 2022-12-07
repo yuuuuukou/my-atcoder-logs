a95860529e65e73b7820fd2f3c50575478e2502b
yuuuuukou <yuuuuukou@gmail.com>
2022-12-07 21:43:54 +0900

    solve: abc196B, 198B

--- src/abc196.kt
+++ src/abc196.kt
@@ -6,7 +6,17 @@ private fun readLong() = readString().toLong()
 private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC196A()
+    solveABC196B()
+}
+
+fun solveABC196B() {
+    val x = readString()
+    val period = x.indexOf(".")
+    if (period != -1) {
+        println(x.substring(0 until period))
+    } else {
+        println(x)
+    }
 }
 
 fun solveABC196A() {
--- src/abc198.kt
+++ src/abc198.kt
@@ -1,3 +1,5 @@
+import kotlin.math.pow
+
 private fun readString() = readLine()!!
 private fun readStrings() = readString().split(" ").toMutableList()
 private fun readInt() = readString().toInt()
@@ -6,7 +8,33 @@ private fun readLong() = readString().toLong()
 private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC198A()
+    solveABC198B()
+}
+
+fun solveABC198B() {
+    var n = readInt()
+
+    var cntZero = 0
+    for (i in 9 downTo 0) {
+        if (n % (10.0.pow(i).toInt()) == 0) {
+            // i = 0 0 0こ
+            // i = 1 0 1こ
+            // i = 2 0 2こ
+            cntZero = i
+            break
+        }
+    }
+
+    var n2 = n.toString()
+    if (cntZero > 0) {
+        n2 = n2.padStart(n2.length + cntZero, '0')
+    }
+
+    if (n2 == n2.reversed()) {
+        println("Yes")
+    } else {
+        println("No")
+    }
 }
 
 fun solveABC198A() {
