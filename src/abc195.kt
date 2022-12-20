5da04497be20ad5dbe96569dd37c745a40538857
yuuuuukou <yuuuuukou@gmail.com>
2022-12-20 21:17:53 +0900

    solve: abc195B, 197B

--- src/abc195.kt
+++ src/abc195.kt
@@ -1,5 +1,3 @@
-import kotlin.math.absoluteValue
-
 private fun readString() = readLine()!!
 private fun readStrings() = readString().split(" ").toMutableList()
 private fun readInt() = readString().toInt()
@@ -8,7 +6,36 @@ private fun readLong() = readString().toLong()
 private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC195A()
+    solveABC195B()
+}
+
+fun solveABC195B() {
+    val (a, b, w) = readInts()
+    val weight = w * 1000
+
+    var max: Int? = null
+    var min: Int? = null
+
+    // 取りうるmaxの個数 w=1000キログラム a=b=1グラム
+    // -> 1,000,000個
+    // -> 1個～1,000,000個に分配可能かを全探索
+    // 1個あたりの重量がa..bにおさまっているかを判定
+    // あまりがでない or あまりをさらに分配したうえでもa..bにおさまっているならOK
+    for (i in 1..1000000) {
+        val mod = weight % i
+
+        val unitWeight = weight / i
+        if (unitWeight in a..b && (unitWeight * i == weight || unitWeight + kotlin.math.max(mod / i, 1) in a..b)) {
+            max = kotlin.math.max(max ?: Int.MIN_VALUE, i)
+            min = kotlin.math.min(min ?: Int.MAX_VALUE, i)
+        }
+    }
+
+    if (max == null && min == null) {
+        println("UNSATISFIABLE")
+    } else {
+        println("$min $max")
+    }
 }
 
 fun solveABC195A() {
--- src/abc197.kt
+++ src/abc197.kt
@@ -6,7 +6,59 @@ private fun readLong() = readString().toLong()
 private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC197A()
+    solveABC197B()
+}
+
+fun solveABC197B() {
+    val (h, w, x, y) = readInts()
+    val s = mutableListOf<String>()
+    repeat(h) {
+        s.add(readString())
+    }
+
+    var horizontalCnt = 0
+    var tmp = 0
+    var flg = 0
+    for (i in 0 until w) {
+        if (i == y - 1) {
+            flg = 1
+        }
+        if (s[x - 1][i] == '#') {
+            if (flg == 1) {
+                horizontalCnt = tmp
+                flg = 0
+            }
+            tmp = 0
+        } else {
+            tmp++
+            if (i == w - 1 && flg == 1) {
+                horizontalCnt = tmp
+            }
+        }
+    }
+
+    var verticalCnt = 0
+    tmp = 0
+    flg = 0
+    for (i in 0 until h) {
+        if (i == x - 1) {
+            flg = 1
+        }
+        if (s[i][y - 1] == '#') {
+            if (flg == 1) {
+                verticalCnt = tmp
+                flg = 0
+            }
+            tmp = 0
+        } else {
+            tmp++
+            if (i == h - 1 && flg == 1) {
+                verticalCnt = tmp
+            }
+        }
+    }
+
+    println(horizontalCnt + verticalCnt - 1)
 }
 
 fun solveABC197A() {
