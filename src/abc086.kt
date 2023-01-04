4ae0bcd72815185ec7d971487d2941524d516705
yuuuuukou <yuuuuukou@gmail.com>
2023-01-05 00:19:30 +0900

    solve: abc086B, 096B

--- src/abc086.kt
+++ src/abc086.kt
@@ -1,22 +1,36 @@
-import kotlin.math.pow
-import kotlin.math.sqrt
+private val reader = System.`in`.bufferedReader()
+private fun readString() = reader.readLine()
+private fun readStrings() = readString().split(" ").toMutableList()
+private fun readInt() = readString().toInt()
+private fun readInts() = readString().split(" ").map { it.toInt() }.toMutableList()
+private fun readLong() = readString().toLong()
+private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
+private fun readDouble() = readString().toDouble()
+private fun readDoubles() = readString().split(" ").map { it.toDouble() }.toMutableList()
+private fun readBigDecimal() = readString().toBigDecimal()
+private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC086A()
+    solveABC086B()
 }
 
 fun solveABC086B() {
-    val ab = readLine()!!.replace(" ", "").toInt()
-    val x = sqrt(ab.toDouble()).toInt()
-    if (x.toDouble().pow(2) == ab.toDouble()) {
-        println("Yes")
-    } else {
-        println("No")
+    val n = readInt()
+    val s = readStrings()
+
+    val map = mutableMapOf<String, Int>()
+    for (si in s) {
+        map[si] = 1
+    }
+
+    when (map.count()) {
+        3 -> println("Three")
+        4 -> println("Four")
     }
 }
 
 fun solveABC086A() {
-    val (a, b) = readLine()!!.split(" ").map { it.toInt() }
+    val (a, b) = readInts()
     if ((a * b) % 2 == 0) {
         println("Even")
     } else {
--- src/abc096.kt
+++ src/abc096.kt
@@ -1,3 +1,5 @@
+import kotlin.math.max
+
 private fun readLn() = readLine()!!
 private fun readStrings() = readLn().split(" ").toMutableList()
 private fun readInt() = readLn().toInt()
@@ -6,7 +8,22 @@ private fun readLong() = readLn().toLong()
 private fun readLongs() = readLn().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC096A()
+    solveABC096B()
+}
+
+fun solveABC096B() {
+    var (a, b, c) = readInts()
+    val k = readInt()
+
+    repeat(k) {
+        when (max(max(a, b), c)) {
+            a -> a *= 2
+            b -> b *= 2
+            c -> c *= 2
+        }
+    }
+
+    println(a + b + c)
 }
 
 fun solveABC096A() {
