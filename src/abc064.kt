c3ea2de7f8175627f774b83caf50a0712ca08a13
yuuuuukou <yuuuuukou@gmail.com>
2023-01-07 04:26:23 +0900

    solve: abc064B, 078B

--- src/abc064.kt
+++ src/abc064.kt
@@ -1,5 +1,24 @@
+private val reader = System.`in`.bufferedReader()
+private fun readString() = reader.readLine()
+private fun readStrings() = readString().split(" ").toMutableList()
+private fun readInt() = readString().toInt()
+private fun readInts() = readString().split(" ").map { it.toInt() }.toMutableList()
+private fun readLong() = readString().toLong()
+private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
+private fun readDouble() = readString().toDouble()
+private fun readDoubles() = readString().split(" ").map { it.toDouble() }.toMutableList()
+private fun readBigDecimal() = readString().toBigDecimal()
+private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
+
 fun main(args: Array<String>) {
-    solveABC064A()
+    solveABC064B()
+}
+
+fun solveABC064B() {
+    val n = readInt()
+    val a = readInts()
+
+    println(a.max()!! - a.min()!!)
 }
 
 fun solveABC064A() {
--- src/abc078.kt
+++ src/abc078.kt
@@ -6,7 +6,24 @@ private fun readLong() = readLn().toLong()
 private fun readLongs() = readLn().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC078A()
+    solveABC078B()
+}
+
+fun solveABC078B() {
+    val (x, y, z) = readInts()
+
+    var tmp = x
+    var cnt = 0
+    while (true) {
+        if (tmp >= y + 2 * z) {
+            tmp -= y + z
+            cnt++
+        } else {
+            break
+        }
+    }
+
+    println(cnt)
 }
 
 fun solveABC078A() {
