7e98ab2e9a684d01040a37bfc7ffe3382c623c18
yuuuuukou <yuuuuukou@gmail.com>
2023-01-17 00:05:37 +0900

    solve: abc103B, 104B 1/16

--- src/abc103.kt
+++ src/abc103.kt
@@ -8,7 +8,23 @@ private fun readLong() = readLn().toLong()
 private fun readLongs() = readLn().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC103A()
+    solveABC103B()
+}
+
+fun solveABC103B() {
+    var s = readLn()
+    val t = readLn()
+
+    var res = false
+    repeat(s.length) {
+        s = s.substring(s.lastIndex) + s.substring(0 until s.lastIndex)
+
+        if (s == t) {
+            res = true
+        }
+    }
+
+    println(if (res) "Yes" else "No")
 }
 
 fun solveABC103A() {
--- src/abc104.kt
+++ src/abc104.kt
@@ -6,7 +6,38 @@ private fun readLong() = readLn().toLong()
 private fun readLongs() = readLn().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC104A()
+    solveABC104B()
+}
+
+fun solveABC104B() {
+    val s = readLn()
+
+    var cntC = 0
+    var res = true
+    for ((i, si) in s.withIndex()) {
+        when {
+            i == 0 -> {
+                if (si != 'A') {
+                    res = false
+                }
+            }
+            i in 2 until s.lastIndex -> {
+                if (si == 'C') {
+                    cntC++
+                }
+            }
+            else -> {
+                if (si.toString() != si.toString().toLowerCase()) {
+                    res = false
+                }
+            }
+        }
+    }
+    if (cntC != 1) {
+        res = false
+    }
+
+    println(if (res) "AC" else "WA")
 }
 
 fun solveABC104A() {
