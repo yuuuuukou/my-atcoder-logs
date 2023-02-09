6495b49669778919ddfead2296211ae26b881329
yuuuuukou <yuuuuukou@gmail.com>
2023-02-10 06:28:12 +0900

    solve: abc017B 2/10

--- src/abc017.kt
+++ src/abc017.kt
@@ -11,7 +11,49 @@ private fun readBigDecimal() = readString().toBigDecimal()
 private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC017A()
+    solveABC017B()
+}
+
+fun solveABC017B() {
+    val x = readString()
+    for ((i, xi) in x.withIndex()) {
+        when (xi) {
+            'c' -> {
+                if (i == x.lastIndex) {
+                    println("NO")
+                    return
+                }
+                val next = x[i + 1]
+                if (next == 'h') {
+                    // ok
+                } else {
+                    println("NO")
+                    return
+                }
+            }
+            'o' -> {}
+            'k' -> {}
+            'u' -> {}
+            'h' -> {
+                if (i == 0) {
+                    println("NO")
+                    return
+                }
+                val prev = x[i - 1]
+                if (prev == 'c') {
+                    // ok
+                } else {
+                    println("NO")
+                    return
+                }
+            }
+            else -> {
+                println("NO")
+                return
+            }
+        }
+    }
+    println("YES")
 }
 
 fun solveABC017A() {
