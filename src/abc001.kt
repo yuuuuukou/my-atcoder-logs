5f2b3ad3ac15f5269bd0678e710e72fc9cce7748
yuuuuukou <yuuuuukou@gmail.com>
2023-02-14 23:13:21 +0900

    solve: abc001B 2/14

--- src/abc001.kt
+++ src/abc001.kt
@@ -10,7 +10,29 @@ private fun readBigDecimal() = readString().toBigDecimal()
 private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC001A()
+    solveABC001B()
+}
+
+fun solveABC001B() {
+    val m = readInt()
+    when (m) {
+        in 0 until 100 -> {
+            println("00")
+        }
+        in 100..5000 -> {
+            val vv = m / 100
+            println(vv.toString().padStart(2, '0'))
+        }
+        in 6000..30000 -> {
+            println(m / 1000 + 50)
+        }
+        in 35000..70000 -> {
+            println((m / 1000 - 30) / 5 + 80)
+        }
+        in 70000..100000 -> {
+            println("89")
+        }
+    }
 }
 
 fun solveABC001A() {
