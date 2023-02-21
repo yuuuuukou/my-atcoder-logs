a8ee66de410803f9edf8ed555b3f957b5b9142f6
yuuuuukou <yuuuuukou@gmail.com>
2023-02-21 21:58:46 +0900

    solve: abc030B 2/21

--- src/abc030.kt
+++ src/abc030.kt
@@ -1,3 +1,6 @@
+import kotlin.math.absoluteValue
+import kotlin.math.min
+
 private val reader = System.`in`.bufferedReader()
 private fun readString() = reader.readLine()
 private fun readStrings() = readString().split(" ").toMutableList()
@@ -11,13 +14,29 @@ private fun readBigDecimal() = readString().toBigDecimal()
 private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC030A()
+    solveABC030B()
+}
+
+fun solveABC030B() {
+    val (n, m) = readInts()
+
+    val short = (n % 12) * 30 + m * 0.5
+    val short2 = (n % 12) * 30 + m * 0.5 + 360
+    val long = m * 6
+    val long2 = m * 6 + 360
+
+    println(
+        min(
+            min((short - long).absoluteValue, (short - long2).absoluteValue),
+            min((short2 - long).absoluteValue, (short2 - long2).absoluteValue)
+        )
+    )
 }
 
 fun solveABC030A() {
     val (a, b, c, d) = readDoubles()
-    val TakahasiWinRate = b/a
-    val AokiWinRate = d/c
+    val TakahasiWinRate = b / a
+    val AokiWinRate = d / c
     when {
         TakahasiWinRate < AokiWinRate -> println("AOKI")
         TakahasiWinRate == AokiWinRate -> println("DRAW")
