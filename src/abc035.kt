8c03efc7051a36ea60e7fc8cf565743a2bab295c
yuuuuukou <yuuuuukou@gmail.com>
2023-02-26 10:47:06 +0900

    solve: abc035B 2/26

--- src/abc035.kt
+++ src/abc035.kt
@@ -1,3 +1,7 @@
+import kotlin.math.absoluteValue
+import kotlin.math.max
+import kotlin.math.min
+
 private val reader = System.`in`.bufferedReader()
 private fun readString() = reader.readLine()
 private fun readStrings() = readString().split(" ").toMutableList()
@@ -11,7 +15,73 @@ private fun readBigDecimal() = readString().toBigDecimal()
 private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC035A()
+    solveABC035B()
+}
+
+fun solveABC035B() {
+    val s = readString()
+    val t = readInt()
+
+    var xMax = 0
+    var yMax = 0
+    var xMin = 0
+    var yMin = 0
+
+    for (si in s) {
+        when (si) {
+            'L' -> {
+                xMax -= 1
+                xMin -= 1
+            }
+            'R' -> {
+                xMax += 1
+                xMin += 1
+            }
+            'U' -> {
+                yMax += 1
+                yMin += 1
+            }
+            'D' -> {
+                yMax -= 1
+                yMin -= 1
+            }
+        }
+    }
+
+    for (si in s) {
+        when (si) {
+            '?' -> {
+                // max
+                val tmpMaxL = (xMax - 1).absoluteValue + (yMax).absoluteValue
+                val tmpMaxR = (xMax + 1).absoluteValue + (yMax).absoluteValue
+                val tmpMaxU = (xMax).absoluteValue + (yMax + 1).absoluteValue
+                val tmpMaxD = (xMax).absoluteValue + (yMax - 1).absoluteValue
+                when (max(max(tmpMaxL, tmpMaxR), max(tmpMaxU, tmpMaxD))) {
+                    tmpMaxL -> xMax -= 1
+                    tmpMaxR -> xMax += 1
+                    tmpMaxU -> yMax += 1
+                    tmpMaxD -> yMax -= 1
+                }
+
+                // min
+                val tmpMinL = (xMin - 1).absoluteValue + (yMin).absoluteValue
+                val tmpMinR = (xMin + 1).absoluteValue + (yMin).absoluteValue
+                val tmpMinU = (xMin).absoluteValue + (yMin + 1).absoluteValue
+                val tmpMinD = (xMin).absoluteValue + (yMin - 1).absoluteValue
+                when (min(min(tmpMinL, tmpMinR), min(tmpMinU, tmpMinD))) {
+                    tmpMinL -> xMin -= 1
+                    tmpMinR -> xMin += 1
+                    tmpMinU -> yMin += 1
+                    tmpMinD -> yMin -= 1
+                }
+            }
+        }
+    }
+
+    when (t) {
+        1 -> println(xMax.absoluteValue + yMax.absoluteValue)
+        2 -> println(xMin.absoluteValue + yMin.absoluteValue)
+    }
 }
 
 fun solveABC035A() {
