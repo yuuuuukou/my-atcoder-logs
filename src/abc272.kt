5092545c4f26657ca5fce26907014dd133b65df9
yuuuuukou <yuuuuukou@gmail.com>
2023-03-08 01:22:05 +0900

    solve: abc272C 3/3

--- src/abc272.kt
+++ src/abc272.kt
@@ -1,3 +1,5 @@
+import kotlin.math.max
+
 private fun readString() = readLine()!!
 private fun readStrings() = readString().split(" ").toMutableList()
 private fun readInt() = readString().toInt()
@@ -10,7 +12,38 @@ private fun readBigDecimal() = readString().toBigDecimal()
 private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC272B()
+    solveABC272C()
+}
+
+fun solveABC272C() {
+    val n = readInt()
+    val a = readInts()
+
+    val even = mutableListOf<Int>()
+    val odd = mutableListOf<Int>()
+    for (ai in a) {
+        if (ai % 2 == 0) {
+            even.add(ai)
+        } else {
+            odd.add(ai)
+        }
+    }
+    even.sortDescending()
+    odd.sortDescending()
+
+    if (even.count() >= 2) {
+        if (odd.count() >= 2) {
+            println(max(even[0] + even[1], odd[0] + odd[1]))
+        } else {
+            println(even[0] + even[1])
+        }
+    } else {
+        if (odd.count() >= 2) {
+            println(odd[0] + odd[1])
+        } else {
+            println(-1)
+        }
+    }
 }
 
 fun solveABC272B() {
