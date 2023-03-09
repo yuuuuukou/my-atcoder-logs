7fc765dc777e33bcaa06b30670c8ca10c328b1be
yuuuuukou <yuuuuukou@gmail.com>
2023-03-10 02:04:12 +0900

    solve: abc217C 3/7

--- src/abc217.kt
+++ src/abc217.kt
@@ -6,7 +6,30 @@ private fun readLong() = readString().toLong()
 private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC217B()
+    solveABC217C()
+}
+
+fun solveABC217C() {
+    val n = readInt()
+    val p = readInts()
+
+    val q = MutableList(1000000) { 0 }
+
+    // i 1 2 3
+    // p 2 3 1
+    // q 3 1 2
+    // 全てのiに対して、qのpi番目の要素がi
+    // i=1 qのp1(=2)番目の要素がi(=1)
+    // i=2 qのp2(=3)番目の要素がi(=2)
+    // i=3 qのp3(=1)番目の要素がi(=3)
+    for (i in 1..n) {
+        q[p[i - 1]] = i
+    }
+
+    for (i in 1..n) {
+        print("${q[i]} ")
+    }
+    println()
 }
 
 fun solveABC217B() {
