95ae78354ea12b45378d8d9fa8cca4e3315ba712
yuuuuukou <yuuuuukou@gmail.com>
2023-03-12 17:52:27 +0900

    solve: abc180C 3/9

--- src/abc180.kt
+++ src/abc180.kt
@@ -10,7 +10,23 @@ private fun readLong() = readString().toLong()
 private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC180B()
+    solveABC180C()
+}
+
+fun solveABC180C() {
+    val n = readLong()
+
+    val list = mutableListOf<Long>()
+    for (i in 1..sqrt(n.toDouble()).toInt()) {
+        if (n % i == 0L) {
+            list.add(i.toLong())
+            list.add(n / i)
+        }
+    }
+
+    for (item in list.toSortedSet()) {
+        println(item)
+    }
 }
 
 fun solveABC180B() {
