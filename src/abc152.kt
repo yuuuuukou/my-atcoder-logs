fa80915d381b7db8e57a483b0073fc593930276f
yuuuuukou <yuuuuukou@gmail.com>
2023-04-22 11:04:27 +0900

    solve: abc152C 4/7

--- src/abc152.kt
+++ src/abc152.kt
@@ -1,3 +1,5 @@
+import kotlin.math.min
+
 private fun readLn() = readLine()!!
 private fun readStrings() = readLn().split(" ").toMutableList()
 private fun readInt() = readLn().toInt()
@@ -6,11 +8,28 @@ private fun readLong() = readLn().toLong()
 private fun readLongs() = readLn().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC152B()
+    solveABC152C()
+}
+
+fun solveABC152C() {
+    val n = readInt()
+    val p = readInts()
+
+    var cnt = 0
+    var min = Int.MAX_VALUE
+    // それまでに自分より小さい値がなければcnt
+    for (pi in p) {
+        if (min >= pi) {
+            cnt++
+        }
+        min = min(min, pi)
+    }
+
+    println(cnt)
 }
 
 fun solveABC152B() {
-    val (a,b) = readStrings()
+    val (a, b) = readStrings()
 
     var x = ""
     repeat(b.toInt()) {
