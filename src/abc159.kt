e6aaa851c077ac6d45a429f21658a15fa70d95b6
yuuuuukou <yuuuuukou@gmail.com>
2023-04-23 18:48:10 +0900

    solve: abc159C 4/10

--- src/abc159.kt
+++ src/abc159.kt
@@ -1,12 +1,27 @@
-private fun readString() = readLine()!!
+private val reader = System.`in`.bufferedReader()
+private fun readString() = reader.readLine()
 private fun readStrings() = readString().split(" ").toMutableList()
 private fun readInt() = readString().toInt()
 private fun readInts() = readString().split(" ").map { it.toInt() }.toMutableList()
 private fun readLong() = readString().toLong()
 private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
+private fun readDouble() = readString().toDouble()
+private fun readDoubles() = readString().split(" ").map { it.toDouble() }.toMutableList()
+private fun readBigDecimal() = readString().toBigDecimal()
+private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
+
 
 fun main(args: Array<String>) {
-    solveABC159B()
+    solveABC159C()
+}
+
+fun solveABC159C() {
+    val l = readDouble()
+
+    val length = l / 3.0
+    val res = length * length * (l - 2 * length)
+
+    println(res)
 }
 
 fun solveABC159B() {
