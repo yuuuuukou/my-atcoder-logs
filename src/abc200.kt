32f61e1176925dfd045f3f6d7c2acc90b702ccb5
yuuuuukou <yuuuuukou@gmail.com>
2023-04-23 22:53:31 +0900

    solve: abc200C 4/14

--- src/abc200.kt
+++ src/abc200.kt
@@ -1,5 +1,34 @@
+private val reader = System.`in`.bufferedReader()
+private fun readString() = reader.readLine()
+private fun readStrings() = readString().split(" ").toMutableList()
+private fun readInt() = readString().toInt()
+private fun readInts() = readString().split(" ").map { it.toInt() }.toMutableList()
+private fun readLong() = readString().toLong()
+private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
+private fun readDouble() = readString().toDouble()
+private fun readDoubles() = readString().split(" ").map { it.toDouble() }.toMutableList()
+private fun readBigDecimal() = readString().toBigDecimal()
+private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
+
 fun main(args: Array<String>) {
-    solveABC200B()
+    solveABC200C()
+}
+
+fun solveABC200C() {
+    val n = readInt()
+    val a = readInts()
+
+    var cnt = 0L
+    val map = mutableMapOf<Int, Int>()
+    // 200で割った余りに変える
+    // 余りが同じ値になる組み合わせは条件を満たす
+    for (ai in a) {
+        val mod = ai % 200
+        cnt += (map[mod] ?: 0)
+        map[mod] = (map[mod] ?: 0) + 1
+    }
+
+    println(cnt)
 }
 
 fun solveABC200B() {
@@ -23,10 +52,10 @@ fun solveABC200A() {
 
     var cnt = 1
     while (n > 0) {
-       n -= 100
-       if (n > 0) {
-           cnt++
-       }
+        n -= 100
+        if (n > 0) {
+            cnt++
+        }
     }
 
     println(cnt)
