8c48c351a10b767bd176894ec44ba17e161046c4
yuuuuukou <yuuuuukou@gmail.com>
2023-05-13 22:08:13 +0900

    solve: abc231C 4/24 Mon.

--- src/abc231.kt
+++ src/abc231.kt
@@ -10,7 +10,35 @@ private fun readBigDecimal() = readString().toBigDecimal()
 private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC231A()
+    solveABC231C()
+}
+
+fun solveABC231C() {
+    val (n, q) = readInts()
+    val a = readInts()
+
+    /**
+     * ref: https://webbibouroku.com/Blog/Article/cs-lowerbound-upperbound
+     */
+    fun lowerBound(list: List<Int>, value: Int): Int {
+        var left = 0
+        var right = list.lastIndex
+        while (left <= right) {
+            val mid = (left + right) / 2
+            if (list[mid] < value) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+        return left
+    }
+
+    val sortedA = a.toMutableList().sorted()
+
+    repeat(q) {
+        println(sortedA.lastIndex - lowerBound(sortedA, readInt()) + 1)
+    }
 }
 
 fun solveABC231B() {
@@ -25,5 +53,5 @@ fun solveABC231B() {
 
 fun solveABC231A() {
     val d = readDouble()
-    println(d/100)
+    println(d / 100)
 }
