c59a9b38e370c6703c34df25c47fa4d4cb09e2e8
yuuuuukou <yuuuuukou@gmail.com>
2023-05-18 04:40:16 +0900

    solve: abc273C 4/26 Wed.

--- src/abc273.kt
+++ src/abc273.kt
@@ -12,7 +12,47 @@ private fun readBigDecimal() = readString().toBigDecimal()
 private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC273B()
+    solveABC273C()
+}
+
+fun solveABC273C() {
+    val n = readInt()
+    val a = readInts()
+    val sortAndDistinctA = a.toMutableList().distinct().sorted()
+
+    /**
+     * ref: https://webbibouroku.com/Blog/Article/cs-lowerbound-upperbound
+     */
+    fun lowerBound(list: List<Int>, value: Int): Int {
+        var left = 0
+        var right = list.lastIndex
+        while (left <= right) {
+            val mid = (left + right) / 2
+            if (list[mid] < value) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+        return left
+    }
+
+    val map = mutableMapOf<Int, Int>()
+    for ((i, ai) in a.withIndex()) {
+        // Aiに関してAに含まれる整数のうちAiより大きいものの種類を数える
+
+        // 例: in 1 2 2 7 8 8    sort distinct 1 2 7 8
+        // - aiが2の場合、aiでlowerBoundする ①
+        val tmp = lowerBound(sortAndDistinctA, ai)
+        // - lastIndex - ①でAiより大きいものの種類が数えられる
+        val tmp2 = sortAndDistinctA.lastIndex - tmp
+
+        map[tmp2] = (map[tmp2] ?: 0) + 1
+    }
+
+    for (k in 0 until n) {
+        println(map[k]?:0)
+    }
 }
 
 fun solveABC273B() {
