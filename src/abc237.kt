758621595e701044ccd72391bfea720a03c69dfb
yuuuuukou <yuuuuukou@gmail.com>
2023-05-21 03:40:08 +0900

    solve: abc237C 5/1 Mon.

--- src/abc237.kt
+++ src/abc237.kt
@@ -13,7 +13,52 @@ private fun readBigDecimal() = readString().toBigDecimal()
 private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC237B()
+    solveABC237C()
+}
+
+fun solveABC237C() {
+    var s = readString()
+
+    var prefixACount = 0
+    for (i in s.indices) {
+        if (s[i] == 'a') {
+            prefixACount++
+        } else {
+            break
+        }
+    }
+
+    var suffixACount = 0
+    for (i in s.lastIndex downTo 0) {
+        if (s[i] == 'a') {
+            suffixACount++
+        } else {
+            break
+        }
+    }
+
+    if (prefixACount == suffixACount) {
+        // そのまま回文判定する
+    } else if (prefixACount < suffixACount) {
+        // 先頭のaが不足、いくらでも足せるので末尾のaは無いものとして考える
+        s = s.reversed().removeRange(0 until (suffixACount - prefixACount))
+    } else {
+        // 末尾のaが不足、無理
+        println("No")
+        return
+    }
+
+    var ok = true
+    for (i in s.indices) {
+        if (s[i] == s[s.lastIndex - i]) {
+            // ok
+        } else {
+            // ng
+            ok = false
+        }
+    }
+
+    println(if (ok) "Yes" else "No")
 }
 
 fun solveABC237B() {
