04ab74cf93e9e25089c0d0e7aa7c7a13cde1c94f
yuuuuukou <yuuuuukou@gmail.com>
2023-05-21 15:54:30 +0900

    solve: abc235C 5/2 Tue.

--- src/abc235.kt
+++ src/abc235.kt
@@ -10,7 +10,26 @@ private fun readBigDecimal() = readString().toBigDecimal()
 private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC235B()
+    solveABC235C()
+}
+
+fun solveABC235C() {
+    val (_, q) = readInts()
+    val a = readInts()
+
+    val countMap = mutableMapOf<Int, Int>()
+    val resultMap = mutableMapOf<Pair<Int, Int>, Int>()
+    for ((i, ai) in a.withIndex()) {
+        // aiが出てきたのが何回目か
+        countMap[ai] = (countMap[ai] ?: 0) + 1
+        // aiのcountMap[ai]はi+1番目
+        resultMap[Pair(ai, countMap[ai]!!)] = i + 1
+    }
+
+    repeat(q) {
+        val (xi, ki) = readInts()
+        println(resultMap[Pair(xi, ki)] ?: -1)
+    }
 }
 
 fun solveABC235B() {
