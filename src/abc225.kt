3bb2cc1c6b0a0c13a44bbec6b409bb2b6080603e
yuuuuukou <yuuuuukou@gmail.com>
2023-05-21 16:40:21 +0900

    solve: abc225C 5/3 Wed.

--- src/abc225.kt
+++ src/abc225.kt
@@ -6,7 +6,45 @@ private fun readLong() = readString().toLong()
 private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC225B()
+    solveABC225C()
+}
+
+fun solveABC225C() {
+    val (n, m) = readInts()
+
+    val b = mutableListOf<MutableList<Int>>()
+
+    // (1,1)から起点の座標を特定
+    val b1 = readInts()
+    // 月1 火2・・・日7
+    // -> 最初がどこから始まっている矩形か確認 = jがわかる
+    val j = if (b1[0] % 7 == 0) 7 else b1[0] % 7
+
+    // b1[0] = (i − 1) * 7 + j
+    // i = ((b1[0] - j) / 7) + 1
+    val i = ((b1[0] - j) / 7) + 1
+
+    b.add(b1)
+
+    repeat(n - 1) {
+        val bi = readInts()
+        b.add(bi)
+    }
+
+    for ((ii, row) in b.withIndex()) {
+        for ((jj, cell) in row.withIndex()) {
+            // 起点の座標を元にindexを補正
+            if ((j + jj) in 1..7 && cell == ((i + ii) - 1) * 7 + (j + jj)) {
+                // ok
+            } else {
+                // ng
+                println("No")
+                return
+            }
+        }
+    }
+
+    println("Yes")
 }
 
 fun solveABC225B() {
