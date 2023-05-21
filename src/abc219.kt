5dffc33eac67c61ddc9bb363ecbcc68143b2dd94
yuuuuukou <yuuuuukou@gmail.com>
2023-05-22 08:31:42 +0900

    solve: abc219C 5/8 Mon.

--- src/abc219.kt
+++ src/abc219.kt
@@ -6,7 +6,61 @@ private fun readLong() = readString().toLong()
 private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC219B()
+    solveABC219C()
+}
+
+fun solveABC219C() {
+    val x = readString()
+    val n = readInt()
+
+    class Name(val name: String, var sortName: String)
+
+    val s = mutableListOf<Name>()
+    repeat(n) {
+        val si = readString()
+        s.add(Name(si, si))
+    }
+
+    for (si in s) {
+        for ((i, xi) in x.withIndex()) {
+            val tmp = when (i) {
+                0 -> "A"
+                1 -> "B"
+                2 -> "C"
+                3 -> "D"
+                4 -> "E"
+                5 -> "F"
+                6 -> "G"
+                7 -> "H"
+                8 -> "I"
+                9 -> "J"
+                10 -> "K"
+                11 -> "L"
+                12 -> "M"
+                13 -> "N"
+                14 -> "O"
+                15 -> "P"
+                16 -> "Q"
+                17 -> "R"
+                18 -> "S"
+                19 -> "T"
+                20 -> "U"
+                21 -> "V"
+                22 -> "W"
+                23 -> "X"
+                24 -> "Y"
+                25 -> "Z"
+                else -> ""
+            }
+            si.sortName = si.sortName.replace(xi.toString(), tmp)
+        }
+    }
+
+    s.sortWith(compareBy { it.sortName })
+
+    for (si in s) {
+        println(si.name)
+    }
 }
 
 fun solveABC219B() {
