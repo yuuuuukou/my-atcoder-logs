5fa3eef3837484031ffe870cfb3ab7d3d37ac044
yuuuuukou <yuuuuukou@gmail.com>
2023-05-21 23:33:16 +0900

    solve: abc223C 5/5 Fri.

--- src/abc223.kt
+++ src/abc223.kt
@@ -1,12 +1,44 @@
-private fun readString() = readLine()!!
+private val reader = System.`in`.bufferedReader()
+private fun readString() = reader.readLine()
 private fun readStrings() = readString().split(" ").toMutableList()
 private fun readInt() = readString().toInt()
 private fun readInts() = readString().split(" ").map { it.toInt() }.toMutableList()
 private fun readLong() = readString().toLong()
 private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
+private fun readDouble() = readString().toDouble()
+private fun readDoubles() = readString().split(" ").map { it.toDouble() }.toMutableList()
+private fun readBigDecimal() = readString().toBigDecimal()
+private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC223B()
+    solveABC223C()
+}
+
+fun solveABC223C() {
+    val n = readInt()
+
+    val list = mutableListOf<Pair<Double, Double>>()
+    var totalTime = 0.0
+    repeat(n) {
+        val (ai, bi) = readDoubles()
+        list.add(Pair(ai, bi))
+        totalTime += (ai / bi)
+    }
+
+    var currentLength = 0.0
+    var currentTime = 0.0
+    for (item in list) {
+        val length = item.first
+        val time = (item.first / item.second)
+        if (totalTime / 2 in currentTime..currentTime + time) {
+            currentLength += (length * (totalTime / 2 - currentTime) / time)
+            println(currentLength)
+            return
+        } else {
+            currentLength += length
+            currentTime += time
+        }
+    }
 }
 
 fun solveABC223B() {
