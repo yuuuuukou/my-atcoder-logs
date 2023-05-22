dcecc4737925fc9956d9f03d5ec6e11b8137ceef
yuuuuukou <yuuuuukou@gmail.com>
2023-05-22 21:06:03 +0900

    solve: abc214C 5/9 Tue.

--- src/abc214.kt
+++ src/abc214.kt
@@ -1,3 +1,5 @@
+import kotlin.math.min
+
 private fun readString() = readLine()!!
 private fun readStrings() = readString().split(" ").toMutableList()
 private fun readInt() = readString().toInt()
@@ -6,7 +8,45 @@ private fun readLong() = readString().toLong()
 private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC214B()
+    solveABC214C()
+}
+
+fun solveABC214C() {
+    val n = readInt()
+    val s = readInts()
+    val t = readInts()
+
+    val res = MutableList(n) { Long.MAX_VALUE }
+
+    for (i in 0 until n) {
+        val beforeIndex = if (i == 0) res.lastIndex else i - 1
+        // i番目の人が直接受け取る時刻
+        val time1 = t[i].toLong()
+        // i番目の人が前の人から受け取る時刻
+        val time2 =
+            if (i == 0) {
+                // 最初の人が前の人から受け取るケースは一旦考えない
+                Long.MAX_VALUE
+            } else {
+                // 前の人が受け取った時間t + 前の人が渡すまでの時間si
+                res[beforeIndex] + s[beforeIndex]
+            }
+        res[i] = min(time1, time2)
+    }
+
+    for (i in 0 until n) {
+        val beforeIndex = if (i == 0) res.lastIndex else i - 1
+        // i番目の人が直接受け取る時刻
+        val time1 = t[i].toLong()
+        // i番目の人が前の人から受け取る時刻
+        // 前の人が受け取った時間t + 前の人が渡すまでの時間si
+        val time2 = res[beforeIndex] + s[beforeIndex]
+        res[i] = min(time1, time2)
+    }
+
+    for (r in res) {
+        println(r)
+    }
 }
 
 fun solveABC214B() {
