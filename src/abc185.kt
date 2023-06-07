6ef7783a3915bacbf85f0cd74a3e853aa5f5b599
yuuuuukou <yuuuuukou@gmail.com>
2023-06-08 08:35:30 +0900

    solve: abc185C 5/16 Tue.

--- src/abc185.kt
+++ src/abc185.kt
@@ -1,8 +1,44 @@
+import java.math.BigDecimal
 import kotlin.math.min
 import kotlin.system.exitProcess
 
+private val reader = System.`in`.bufferedReader()
+private fun readString() = reader.readLine()
+private fun readStrings() = readString().split(" ").toMutableList()
+private fun readInt() = readString().toInt()
+private fun readInts() = readString().split(" ").map { it.toInt() }.toMutableList()
+private fun readLong() = readString().toLong()
+private fun readLongs() = readString().split(" ").map { it.toLong() }.toMutableList()
+private fun readDouble() = readString().toDouble()
+private fun readDoubles() = readString().split(" ").map { it.toDouble() }.toMutableList()
+private fun readBigDecimal() = readString().toBigDecimal()
+private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
+
 fun main() {
-    solveABC185B()
+    solveABC185C()
+}
+
+fun solveABC185C() {
+    val l = readBigDecimal()
+    // l - 1 から 11個選ぶ選び方？
+    // 2^63未満・・・Longにおさまる？
+
+    // 12! / ( 11! * (12-11)! ) = 12
+    // 16! / ( 11! * (16-11)! ) =
+
+    fun factorial(num: BigDecimal): BigDecimal {
+        if (num <= 1.toBigDecimal()) {
+            return 1.toBigDecimal()
+        }
+
+        return num * factorial(num - 1.toBigDecimal())
+    }
+
+    val res =
+        factorial(l - 1.toBigDecimal()) /
+                (factorial(11.toBigDecimal()) * factorial(l - 1.toBigDecimal() - 11.toBigDecimal()))
+
+    println(res)
 }
 
 fun solveABC185B() {
