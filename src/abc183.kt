75c4a4e97e9542aa44cbb6e9eda13d097b2df526
yuuuuukou <yuuuuukou@gmail.com>
2023-06-16 04:53:56 +0900

    solve: abc183C 5/17 Wed.

--- src/abc183.kt
+++ src/abc183.kt
@@ -11,7 +11,69 @@ private fun readBigDecimal() = readString().toBigDecimal()
 private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC183B()
+    solveABC183C()
+}
+
+fun solveABC183C() {
+    val (n, k) = readInts()
+    val t = mutableListOf<MutableList<Int>>()
+    repeat(n) {
+        val ti = readInts()
+        t.add(ti)
+    }
+
+    /**
+     * next_permutation
+     * 受け取ったリストの次の順列にarrayを更新する
+     * [1, 2, 3, 4, 5] -> [1, 2, 3, 5, 4]
+     *
+     * コードの参考: https://koboshi-kyopro.hatenablog.com/entry/2021/07/21/193611
+     * コメントの参考: https://qiita.com/Nikkely/items/0ddca51b3c0e60afbaab
+     */
+    fun nextPermutation(array: MutableList<Int>): Boolean {
+        // i: array[i] < array[i + 1]を満たすもののうちインデックス最大のものを探す。のでdownToでループ
+        for (i in array.lastIndex - 1 downTo 0) {
+            if (array[i] < array[i + 1]) {
+                // j: 末尾から探索して初めて現れるarray[i]より値が大きい要素のインデックスを探す。
+                for (j in array.lastIndex downTo i + 1) {
+                    if (array[j] > array[i]) {
+                        // 見つけた。array[i]とarray[j]を入れ替える
+                        val tmp = array[i]
+                        array[i] = array[j]
+                        array[j] = tmp
+
+                        // i+1以降の要素を昇順に並べ替える
+                        // i番目までは入れ替え済みor関係ないので、i+1番目からをtakeLastで取得してソート
+                        val tmpArray = array.takeLast(array.size - (i + 1)).sorted()
+                        tmpArray.forEachIndexed { index, e ->
+                            // tmpArrayのindex == arrayのi+1+indexなので、ソート済みの値で上書きしていく
+                            array[i + 1 + index] = e
+                        }
+
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+
+    var res = 0
+    val permutation = MutableList(n - 1) { i -> i + 2 }
+    do {
+        var sum = 0
+        val path = MutableList(1) { 1 }
+        path.addAll(permutation)
+        for (i in path.indices) {
+            val from = path[i]
+            val to = if (i == path.lastIndex) 1 else path[i + 1]
+
+            sum += t[from - 1][to - 1]
+        }
+        if (sum == k) res++
+    } while (nextPermutation(permutation))
+
+    println(res)
 }
 
 fun solveABC183B() {
