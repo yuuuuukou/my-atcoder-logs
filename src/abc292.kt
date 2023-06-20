c953c84ecf2851cb1c00388aa09b0f881ac8e976
yuuuuukou <yuuuuukou@gmail.com>
2023-06-21 08:19:16 +0900

    solve: abc292B, 295B 5/26 Fri.

--- src/abc292.kt
+++ src/abc292.kt
@@ -11,7 +11,22 @@ private fun readBigDecimal() = readString().toBigDecimal()
 private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC292A()
+    solveABC292B()
+}
+
+fun solveABC292B() {
+    val (n, q) = readInts()
+
+    val cards = MutableList(n + 1) { 0 }
+
+    repeat(q) {
+        val event = readInts()
+        when (event[0]) {
+            1 -> cards[event[1]] += 1
+            2 -> cards[event[1]] += 2
+            3 -> println(if (cards[event[1]] >= 2) "Yes" else "No")
+        }
+    }
 }
 
 fun solveABC292A() {
--- src/abc295.kt
+++ src/abc295.kt
@@ -1,3 +1,5 @@
+import kotlin.math.absoluteValue
+
 private val reader = System.`in`.bufferedReader()
 private fun readString() = reader.readLine()
 private fun readStrings() = readString().split(" ").toMutableList()
@@ -11,7 +13,37 @@ private fun readBigDecimal() = readString().toBigDecimal()
 private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC295A()
+    solveABC295B()
+}
+
+fun solveABC295B() {
+    val (r, c) = readInts()
+    val b = mutableListOf<CharArray>()
+    repeat(r) {
+        b.add(readString().toCharArray())
+    }
+
+    for (row in 0 until r) {
+        for (col in 0 until c) {
+            val cell = b[row][col].toString()
+            if (cell in "1".."9") {
+                for (i in 0 until r) {
+                    for (j in 0 until c) {
+                        // 爆弾は先に消しちゃうと爆発処理が漏れるので処理するタイミングで消しこむ
+                        if ((row != i || col != j) && b[i][j].toString() in "1".."9") continue
+
+                        if ((row - i).absoluteValue + (col - j).absoluteValue <= cell.toInt()) {
+                            b[i][j] = '.'
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (i in 0 until r) {
+        println(b[i].joinToString(""))
+    }
 }
 
 fun solveABC295A() {
