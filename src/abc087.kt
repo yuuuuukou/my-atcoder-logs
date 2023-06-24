64a89d27447d8ada5f08f81acc263032d3e46af8
yuuuuukou <yuuuuukou@gmail.com>
2023-06-25 03:16:30 +0900

    solve: abc087C 6/4 Sun.

--- src/abc087.kt
+++ src/abc087.kt
@@ -1,3 +1,5 @@
+import kotlin.math.max
+
 private fun readLn() = readLine()!!
 private fun readStrings() = readLn().split(" ").toMutableList()
 private fun readInt() = readLn().toInt()
@@ -6,7 +8,41 @@ private fun readLong() = readLn().toLong()
 private fun readLongs() = readLn().split(" ").map { it.toLong() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC087A()
+    solveABC087C()
+}
+
+fun solveABC087C() {
+    val n = readInt()
+    val a1 = readInts()
+    val a2 = readInts()
+
+    val a1Sum = mutableListOf<Int>()
+    for (ai in a1) {
+        if (a1Sum.isNotEmpty()) {
+            a1Sum.add(a1Sum[a1Sum.lastIndex] + ai)
+        } else {
+            a1Sum.add(ai)
+        }
+    }
+    val a2Sum = mutableListOf<Int>()
+    for (ai in a2) {
+        if (a2Sum.isNotEmpty()) {
+            a2Sum.add(a2Sum[a2Sum.lastIndex] + ai)
+        } else {
+            a2Sum.add(ai)
+        }
+    }
+
+    var res = 0
+    for (i in 0 until n) {
+        var tmp = a1Sum[i] + a2Sum[a2Sum.lastIndex]
+        if (i != 0) {
+            tmp -= a2Sum[i - 1]
+        }
+        res = max(res, tmp)
+    }
+
+    println(res)
 }
 
 fun solveABC087B() {
