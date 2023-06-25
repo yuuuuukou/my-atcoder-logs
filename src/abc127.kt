e7964636d975fe647abe2d5dcae22a1ab56f2be9
yuuuuukou <yuuuuukou@gmail.com>
2023-06-25 20:45:50 +0900

    solve: abc127C 6/13 Tue.

--- src/abc127.kt
+++ src/abc127.kt
@@ -8,7 +8,30 @@ private fun readDouble() = readLn().toDouble()
 private fun readDoubles() = readLn().split(" ").map { it.toDouble() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC127B()
+    solveABC127C()
+}
+
+fun solveABC127C() {
+    val (n, m) = readInts()
+
+    var left = 1
+    var right = n
+    repeat(m) {
+        val (li, ri) = readInts()
+
+        if (li in left..right) {
+            left = li
+        }
+        if (ri in left..right) {
+            right = ri
+        }
+        if (ri < left || li > right) {
+            println(0)
+            return
+        }
+    }
+
+    println((left..right).toSet().count())
 }
 
 fun solveABC127B() {
