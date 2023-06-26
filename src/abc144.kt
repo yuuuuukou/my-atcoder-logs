3cbfc572bdd2816718c51b4883b8711dac99b1ec
yuuuuukou <yuuuuukou@gmail.com>
2023-06-26 20:04:09 +0900

    solve: abc144C 6/19 Mon.

--- src/abc144.kt
+++ src/abc144.kt
@@ -1,3 +1,5 @@
+import kotlin.math.min
+
 private fun readLn() = readLine()!!
 private fun readStrings() = readLn().split(" ").toMutableList()
 private fun readInt() = readLn().toInt()
@@ -8,7 +10,39 @@ private fun readDouble() = readLn().toDouble()
 private fun readDoubles() = readLn().split(" ").map { it.toDouble() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC144A()
+    solveABC144C()
+}
+
+fun solveABC144C() {
+    val n = readLong()
+
+    // ref: https://qiita.com/drken/items/a14e9af0ca2d857dad23#3-%E7%B4%84%E6%95%B0%E5%88%97%E6%8C%99
+    fun getDivisors(n: Long): MutableList<Long> {
+        val res = mutableListOf<Long>()
+
+        var i = 1L
+        while (i * i <= n) {
+            if (n % i == 0L) {
+                res.add(i)
+                if (n / i != i) res.add(n / i)
+            }
+            i++
+        }
+        res.sort()
+        return res
+    }
+
+    var res = Long.MAX_VALUE
+    val divisors = getDivisors(n)
+    for (i in divisors) {
+        for (j in divisors) {
+            if (i * j == n) {
+                res = min(i + j, res)
+            }
+        }
+    }
+
+    println(res - 2)
 }
 
 fun solveABC144B() {
