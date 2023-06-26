176f82cfaae22c2e94a079886ece76b5abc308e4
yuuuuukou <yuuuuukou@gmail.com>
2023-06-26 22:19:51 +0900

    solve: abc304C 6/23 Fri.

--- src/abc304.kt
+++ src/abc304.kt
@@ -11,7 +11,53 @@ private fun readBigDecimal() = readString().toBigDecimal()
 private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC304B()
+    solveABC304C()
+}
+
+fun solveABC304C() {
+    val (n, d) = readInts()
+
+    class Person(val x: Int, val y: Int)
+
+    val persons = mutableMapOf<Int, Person>()
+    for (i in 0 until n) {
+        val (xi, yi) = readInts()
+        persons[i] = Person(xi, yi)
+    }
+
+    val virus = MutableList(n) { 0 }
+    virus[0] = 1
+    val nextCheck = mutableSetOf<Int>()
+    nextCheck.add(0)
+    val checked = mutableSetOf<Int>()
+    while (nextCheck.isNotEmpty()) {
+        val tmp = nextCheck.toMutableList()
+        for (checkPerson in tmp) {
+            for (i in 0 until n) {
+                val person = persons[i]
+                if (checked.contains(i)) continue
+
+                val a1 = persons[checkPerson]!!.x
+                val a2 = persons[checkPerson]!!.y
+                val b1 = person!!.x
+                val b2 = person!!.y
+                if (d * d >= (a1 - b1) * (a1 - b1) + (a2 - b2) * (a2 - b2)) {
+                    virus[i] = 1
+                    nextCheck.add(i)
+                }
+            }
+            nextCheck.remove(checkPerson)
+            checked.add(checkPerson)
+        }
+    }
+
+    for (i in 0 until n) {
+        if (virus[i] == 1) {
+            println("Yes")
+        } else {
+            println("No")
+        }
+    }
 }
 
 fun solveABC304B() {
