307abc5d84fbb370a0842cb4de82784da3f56adc
yuuuuukou <yuuuuukou@gmail.com>
2023-06-26 21:28:36 +0900

    solve: abc301C 6/22 Thu.

--- src/abc301.kt
+++ src/abc301.kt
@@ -13,7 +13,53 @@ private fun readBigDecimal() = readString().toBigDecimal()
 private fun readBigDecimals() = readString().split(" ").map { it.toBigDecimal() }.toMutableList()
 
 fun main(args: Array<String>) {
-    solveABC301B()
+    solveABC301C()
+}
+
+fun solveABC301C() {
+    val s = readString()
+    val t = readString()
+
+    val sMap = mutableMapOf<Char, Int>()
+    for (si in s) {
+        sMap[si] = (sMap[si] ?: 0) + 1
+    }
+    val tMap = mutableMapOf<Char, Int>()
+    for (ti in t) {
+        tMap[ti] = (tMap[ti] ?: 0) + 1
+    }
+
+    var difference = 0
+    for (c in 'a'..'z') {
+        var tmpDiff = ((sMap[c] ?: 0) - (tMap[c] ?: 0)).absoluteValue
+        if ((sMap[c] ?: 0) < (tMap[c] ?: 0)) {
+            // s側で不足、@で補填できるか確認
+            if (c in arrayOf('a', 't', 'c', 'o', 'd', 'e', 'r')) {
+                if ((sMap['@'] ?: 0) >= tmpDiff) {
+                    sMap['@'] = (sMap['@'] ?: 0) - tmpDiff
+                    tmpDiff = 0
+                }
+            }
+        } else if ((sMap[c] ?: 0) == (tMap[c] ?: 0)) {
+            // ok
+        } else if ((sMap[c] ?: 0) > (tMap[c] ?: 0)) {
+            // t側で不足、@で補填できるか確認
+            if (c in arrayOf('a', 't', 'c', 'o', 'd', 'e', 'r')) {
+                if ((tMap['@'] ?: 0) >= tmpDiff) {
+                    tMap['@'] = (tMap['@'] ?: 0) - tmpDiff
+                    tmpDiff = 0
+                }
+            }
+        }
+
+        difference += tmpDiff
+    }
+
+    if (difference == 0) {
+        println("Yes")
+    } else {
+        println("No")
+    }
 }
 
 fun solveABC301B() {
